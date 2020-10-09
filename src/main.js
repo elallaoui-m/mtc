c5eef0b28a8cbcbd668413ba0adc3b637fc959e7
ELALLAOUI <elallaoui.elmehdi@gmail.com>
2020-10-09 14:12:07 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/Home', component: App },
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
